7658c04a580296721980d1d39d3f8865b97847e0
Hailey <haileyrpan@gmail.com>
2024-07-06 12:21:10 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: 'haileys-camera-roll', // Replace <REPO_NAME> with your actual repo name
+  base: '/', // Base URL for the custom domain
   build: {
     outDir: 'dist',
   },
